cb9b2008490f8c91101829db9265639a3307ac6a
carolinaporto <mariacp9@al.insper.edu.br>
2026-08-20 01:56:43 -0300

    Update last updated date in content.ts

--- src/data/content.ts
+++ src/data/content.ts
@@ -13,8 +13,8 @@ const PIX_KEY = 'f5109849-8c38-42b3-8328-6e86fa7a10aa'; //nubank
 
 // ─── Atualizar aqui quando o valor arrecadado mudar ───────────────────────────
 const lastUpdated = {
-  pt: '30 de Julho de 2026',
-  en: 'July 30, 2026',
+  pt: '19 de Agosto de 2026',
+  en: 'August 19, 2026',
 };
 // ─────────────────────────────────────────────────────────────────────────────
 
